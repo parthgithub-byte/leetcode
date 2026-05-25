5ba6657598bfde9dbada30484c2d19759b12fbfb
Parth Pakhare <parthpakhareducation@gmail.com>
2026-05-25 20:46:32 +0530

    Time: 0 ms (100%), Space: 29.6 MB (65.57%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+        int i = 0, j = n-k-1;
+        while (i<j){
+            swap(nums[i], nums[j]);
+            i++; j--;
+        }
+
+        i = n-k, j = n-1;
+        while (i<j){
+            swap(nums[i], nums[j]);
+            i++; j--;
+        }
+
+        i = 0, j = n-1;
+        while (i<j){
+            swap(nums[i], nums[j]);
+            i++; j--;
+        }
+
+    }
+};
