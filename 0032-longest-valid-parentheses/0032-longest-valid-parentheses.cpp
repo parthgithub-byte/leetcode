47bb2c6b7edca1f4e880723b9d3f5a0a63e02019
Parth Pakhare <parthpakhareducation@gmail.com>
2026-06-13 09:38:34 +0530

    Time: 3 ms (39.79%), Space: 13.3 MB (5.86%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,42 +2,34 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
         int n = s.size();
+        vector<int>sequence(n, 0);
         stack<int>st;
-        vector<int>sequence(n,0);
 
         for (int i=0; i<n; i++){
-            if (!st.empty()){
-                if (s[i] == ')'){
-                    if (s[st.top()] == '('){
-                        sequence[st.top()] = 1;
-                        sequence[i] = 1;
-                        st.pop();
-                    }
-                    else{
-                        // unstisfied s[i]
-                        st.push(i);
-                    }
-                }
-                else{
-                    // again, couldn't satisfy this one like in the ideal case
-                    st.push(i);
-                }
+            if (!st.empty() && s[st.top()] == '(' && s[i] == ')'){
+                sequence[st.top()] = 1;
+                sequence[i] = 1;
+                st.pop();
             }
             else{
+                // else if, st was empty or s[st.top()] was not '(' or s[i] was not ')'
                 st.push(i);
             }
         }
 
-        int maxi = 0, count = 0;
+/* Eg:
+")(()))()" gives the sequence:
+ 01111011
+*/
+        int maxi=0, count=0;
         for (int i=0; i<n; i++){
-            if (sequence[i] == 0){
-                count = 0;
+            if (sequence[i]==0){
+                count=0;
             }
-            else {
+            else{
                 count++;
                 maxi = max(maxi, count);
             }
-            // maxi += sequence[i];
         }
 
         return maxi;
