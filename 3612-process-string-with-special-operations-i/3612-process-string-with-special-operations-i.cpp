6c44fa2086438f3972752faeb1bffacce5d008c3
Parth Pakhare <parthpakhareducation@gmail.com>
2026-06-16 23:41:10 +0530

    Time: 7 ms (35.61%), Space: 39.9 MB (42.05%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string result;
+        int n = s.size();
+        for (int i=0; i<n; i++){
+            if (s[i]=='*'){
+                if (result.size() != 0){
+                    result.pop_back();
+                }
+            }
+            else if (s[i]=='#'){
+                result += result;
+            }
+            else if (s[i]=='%'){
+                reverse(result.begin(), result.end());
+            }
+            else{
+                result.push_back(s[i]);
+            }
+        }
+
+        return result;
+    }
+};
