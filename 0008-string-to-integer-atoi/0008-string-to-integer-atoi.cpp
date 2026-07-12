a952038cacca497db42bc5d1310e3081e2e3b119
Parth Pakhare <parthpakhareducation@gmail.com>
2026-07-12 14:51:21 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.65%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0;
+        int n = s.size();
+        int sign = 1;
+        long long result = 0;
+        
+        while (i<n && s[i]==' '){
+            i++;
+        }
+
+        if (i<n && (s[i] == '-' || s[i] == '+')){
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+
+        while (i<n && s[i]>='0' && s[i]<='9'){
+            result = result*10 + (s[i] - '0');
+
+            if (sign==1 && result >= INT_MAX)   return INT_MAX;
+            if (sign==-1 && -result <= INT_MIN)   return INT_MIN;
+
+            i++;
+        }
+
+        return result*sign;
+    }
+};
