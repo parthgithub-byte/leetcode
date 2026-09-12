967a35e13f6f50ac6d312c15c8d6bb0784c47c45
Parth Pakhare <parthpakhareducation@gmail.com>
2026-09-12 14:48:04 +0530

    Time: 0 ms (100%), Space: 9.1 MB (79.06%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-    void reverseString(string &s, int len){
-        int f = 0, l = len-1;
-        while (f<l){
-            swap(s[f], s[l]);
-            f++; l--;
-        }
-    }
-
+    
     string addStrings(string num1, string num2) {
         string ans;
 
-        string temp;
-        if (num1.size() < num2.size()){ 
-            // we are strictly assuming num1 to be of more length
-            temp = num1;
-            num1 = num2;
-            num2 = temp;
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+        if (num1.size()<num2.size()){
+            swap(num1, num2);
         }
 
-        reverseString(num1, num1.size());
-        reverseString(num2, num2.size());
+        int n1=num1.size();
+        int n2=num2.size();
 
-        int carry = 0, i=0;
-        for (; i<num2.size(); i++){
-            int sum = carry + (num1[i] - '0') + (num2[i] - '0');
-            ans += ((sum%10) + '0');
-            carry = sum / 10;
-        }
-
-        while (i<num1.size()){
-            int sum = carry + (num1[i] - '0');
-            ans += ((sum%10) + '0');
-            carry = sum / 10;
+        int carry=0, i=0;
+        while(i<n2){
+            int sum=(num1[i]-'0')+(num2[i]-'0')+carry;
+            char c=(sum%10 +'0');
+            carry=sum/10;
+            ans.push_back(c);
             i++;
         }
 
-        if (carry!=0)
-            ans += (carry + '0');
+        while(i<n1){
+            int sum=(num1[i]-'0')+carry;
+            char c=sum%10 + '0';
+            carry=sum/10;
+            ans.push_back(c);
+            i++;
+        }
+        if (carry!=0){
+            char c=carry+'0';
+            ans.push_back(c);
+        }
 
-        reverseString(ans, ans.size());
-
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
