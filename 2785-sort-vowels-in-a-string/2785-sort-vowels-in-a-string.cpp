361ffc3d1e79f7948c371048be80c48bc47d81fc
Parth Pakhare <parthpakhareducation@gmail.com>
2026-09-12 14:33:10 +0530

    Time: 11 ms (78.7%), Space: 15.5 MB (92.94%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+            c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
     string sortVowels(string s) {
         string vowels;
         for (auto &c: s){
-            if (c=='a' ||c=='e' ||c=='i' ||c=='o' ||c=='u' ||c=='A' ||c=='E' ||c=='I' ||c=='O' ||c=='U'){
-                vowels+=c;
-                c = '0';
+            if (isVowel(c)){
+                vowels.push_back(c);
+                c='0';
             }
         }
 
         sort(vowels.begin(), vowels.end());
 
-        int curr = 0;
-        for (auto &c : s){
-            if (c == '0'){
-                c = vowels[curr];
-                curr++;
+        int i=0;
+        for (auto &c: s){
+            if(c=='0'){
+                c=vowels[i];
+                i++;
             }
         }
 
